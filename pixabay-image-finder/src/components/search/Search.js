b617d97d578814042b42d1f0c1b48537af819ede
Shannon King <kingshannon01@gmail.com>
2019-10-07 13:23:52 -0700

    if value is an empty string,an empty array of images is the result, else axios fetches imgs based on searchText input requests

--- pixabay-image-finder/src/components/search/Search.js
+++ pixabay-image-finder/src/components/search/Search.js
@@ -3,7 +3,6 @@ import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
 import axios from "axios";
-import ImageResults from "../image-results/ImageResults";
 
 class Search extends Component {
   state = {
@@ -14,16 +13,24 @@ class Search extends Component {
     images: []
   };
 
-  onTextChange = (e) => {
-    this.setState({[e.target.name]: e.target.value}, ()
-    => {
-        axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
-        .then(res => this.setState({images: res.data.hits}))
-        .catch(err => console.log(err));
+  onTextChange = e => {
+    const val = e.target.value;
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === " ") {
+        this.setState({ images: [] });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => console.log(err));
+      }
     });
-  }; 
+  };
 
   render() {
+    console.log(this.state.images);
     return (
       <div>
         <TextField
@@ -49,7 +56,7 @@ class Search extends Component {
         <br />
       </div>
     );
-  };
+  }
 }
 
 export default Search;
