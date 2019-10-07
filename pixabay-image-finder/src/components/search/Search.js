8281b83fd8618fa45a697f44a8dca708b24d4a7a
Shannon King <kingshannon01@gmail.com>
2019-10-07 13:53:31 -0700

    ternary operator to show ImageResults component based on a length grater than 0 otherwise result is null

--- pixabay-image-finder/src/components/search/Search.js
+++ pixabay-image-finder/src/components/search/Search.js
@@ -3,6 +3,7 @@ import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
 import axios from "axios";
+import ImageResults from "../image-results/ImageResults";
 
 class Search extends Component {
   state = {
@@ -56,6 +57,10 @@ class Search extends Component {
           <MenuItem value={50} primaryText="50" />
         </SelectField>
         <br />
+        {/* Ternary operator below */}
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
       </div>
     );
   }
