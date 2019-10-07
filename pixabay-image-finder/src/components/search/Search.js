e05f68f798ca269b98131d54c2235e521e6f0e10
Shannon King <kingshannon01@gmail.com>
2019-10-07 13:34:27 -0700

    created onAmountChange function

--- pixabay-image-finder/src/components/search/Search.js
+++ pixabay-image-finder/src/components/search/Search.js
@@ -29,6 +29,8 @@ class Search extends Component {
     });
   };
 
+  onAmountChange = (e, index, value) => this.setState({ amount: value });
+
   render() {
     console.log(this.state.images);
     return (
