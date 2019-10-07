ff2f77a5bb4286723305e1f7c938929322b3cfef
Shannon King <kingshannon01@gmail.com>
2019-10-07 12:36:54 -0700

    amount of images,  menuItems added

--- pixabay-image-finder/src/components/search/Search.js
+++ pixabay-image-finder/src/components/search/Search.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
+import axios from "axios";
+import ImageResults from "../image-results/ImageResults";
 
 class Search extends Component {
   state = {
@@ -14,13 +17,28 @@ class Search extends Component {
   render() {
     return (
       <div>
-        <TextField 
-            name="searchText"
-            value={this.state.searchText}
-            onChange={this.onTextChange}
-            floatingLabelText="Search for Images"
-            fullWidth={true}
+        <TextField
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search for Images"
+          fullWidth={true}
         />
+        <br />
+        <SelectField
+          name="amount"
+          floatingLabelText="Amount"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        >
+          <MenuItem value={5} primaryText="5" />
+          <MenuItem value={10} primaryText="10" />
+          <MenuItem value={15} primaryText="15" />
+          <MenuItem value={30} primaryText="30" />
+          <MenuItem value={50} primaryText="50" />
+        </SelectField>
+        <br />
+       
       </div>
     );
   }
